68e812c46b2ae3298267a2765931918f53529396
shivaswaroop40 <shivaswaroop40@gmail.com>
2026-02-20 11:09:02 +0530

    added missing blog section

--- script.js
+++ script.js
@@ -103,7 +103,7 @@ const observer = new IntersectionObserver((entries) => {
 }, observerOptions);
 
 // Observe elements
-document.querySelectorAll('.experience-item, .project, .speaking-item, .education-item').forEach(el => {
+document.querySelectorAll('.experience-item, .project, .speaking-item, .education-item, .blog-card').forEach(el => {
     el.style.opacity = '0';
     el.style.transform = 'translateY(20px)';
     el.style.transition = 'opacity 0.6s ease, transform 0.6s ease';
